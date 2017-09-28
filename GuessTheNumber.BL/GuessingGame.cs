cb19e1dfe92133787c0c93515c63b790846ffebd
Christer Carlsson <ccarlsson@outlook.com>
2017-09-28 10:59:05 +0200

    Add BL

---
+++ GuessTheNumber.BL/GuessingGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber.BL
+{
+    public class GuessingGame
+    {
+        Random _randomizer;
+        int _secretNumber;
+        public int NumberOfGuesses { get; set; }
+
+        public GuessingGame()
+        {
+            _randomizer = new Random();
+            InitialGame();
+        }
+
+        private void InitialGame()
+        {
+            _secretNumber = _randomizer.Next(1, 101);
+        }
+
+        public int Guess(int guessedNumber)
+        {
+            NumberOfGuesses++;
+            return guessedNumber.CompareTo(_secretNumber);
+        }
+    }
+}
--- GuessTheNumber/MainWindow.xaml.cs
+++ GuessTheNumber/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuessTheNumber.BL;
+using System;
 using System.Windows;
 
 namespace GuessTheNumber
@@ -8,56 +9,58 @@ namespace GuessTheNumber
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random _randomizer;
-        int _secretNumber;
-        int _numberOfGuesses;
+
+
+        GuessingGame game;
         int _highScore;
 
         public MainWindow()
         {
             InitializeComponent();
-            _randomizer = new Random();
+            
             _highScore = int.MaxValue;
             InitializeGame();
         }
 
         private void InitializeGame()
         {
-            _secretNumber = _randomizer.Next(1, 101);
+            game = new GuessingGame();
+            OutputTextBlock.Text = "Start guessing a number";
             GuessButton.Visibility = Visibility.Visible;
             RunAgainButton.Visibility = Visibility.Hidden;
         }
 
         private void Guess_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(InputTextBox.Text, out int guessedNumber))
+            if(int.TryParse(InputTextBox.Text, out int guessedNumber))
             {
-                _numberOfGuesses++;
-                CheckIfRight(guessedNumber);
-                return;
+                int result = game.Guess(guessedNumber);
+                if(result < 0)
+                {
+                    OutputTextBlock.Text = "You need to guess higher";
+                } 
+                else if(result > 0)
+                {
+                    OutputTextBlock.Text = "You need to guess lower";
+                }
+                else
+                {
+                    HandleSuccess();
+                }
             }
-            OutputTextBlock.Text = $"You need to enter a number"; 
+            else
+            {
+                OutputTextBlock.Text = "You can only enter a number";
+            }
         }
 
         private void CheckIfRight(int guessedNumber)
         {
-            if(guessedNumber < _secretNumber )
-            {
-                OutputTextBlock.Text = $"You need to guess higher.";
-            }
-            else if(guessedNumber > _secretNumber)
-            {
-                OutputTextBlock.Text = $"You need to guess lower";
-            }
-            else
-            {
-                HandleSuccess();
-            }
         }
 
         private void HandleSuccess()
         {
-            OutputTextBlock.Text = $"Well done, you guessed it with {_numberOfGuesses} tries..";
+            OutputTextBlock.Text = $"Well done, you guessed it with {game.NumberOfGuesses} tries..";
             CheckIfHighScore();
             GuessButton.Visibility = Visibility.Hidden;
             RunAgainButton.Visibility = Visibility.Visible;
@@ -65,9 +68,9 @@ namespace GuessTheNumber
 
         private void CheckIfHighScore()
         {
-            if(_numberOfGuesses < _highScore)
+            if(game.NumberOfGuesses < _highScore)
             {
-                _highScore = _numberOfGuesses;
+                _highScore = game.NumberOfGuesses;
                 HighScoreTextBlock.Text = $"High Score: {_highScore}";
             }
 
