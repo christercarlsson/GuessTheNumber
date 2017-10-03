a0a5592766a7ea42cba612b32b6bac99fcabcc7d
Christer Carlsson <ccarlsson@outlook.com>
2017-10-03 12:14:48 +0200

    Rename Command

--- GuessTheNumber.VM/MainWindowViewModel.cs
+++ GuessTheNumber.VM/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuessTheNumber.BL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,6 +10,24 @@ namespace GuessTheNumber.VM
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        
+        public MainWindowViewModel()
+        {
+            GuessCommand = new RelayCommand(Guess, CanGuess);
+            RestartCommand = new RelayCommand(Restart);
+            InitGame();
+        }
+
+       
+
+        private GuessingGame game;
+
+        public bool GameOn { get; set; }
+        public RelayCommand GuessCommand { get; private set; }
+        public RelayCommand RestartCommand { get; private set; }
+        public bool GuessVisible { get { return GameOn; } }
+        public bool RestartVisible { get { return !GameOn; } }
+
         private string _message;
 
         public string Message
@@ -19,16 +38,51 @@ namespace GuessTheNumber.VM
             }
         }
 
-        private string _output;
+        private string _input;
 
-        public string Output
+        public string Input
         {
-            get { return _output; }
+            get { return _input; }
             set {
-                SetProperty(value, ref _output, nameof(Output));
+                SetProperty(value, ref _input, nameof(Input));
+                GuessCommand.RaiseCanExecuteChanged(); 
             }
         }
 
+        private void Guess(object obj)
+        {
+            int result = game.Guess(int.Parse(Input));
+            if (result == 0)
+                SuccessfullGuess();
+            else
+                Message = result > 0 ? "You need to guess lower" : "You need to guess higher";
+        }
+
+        private bool CanGuess(object obj)
+        {
+            return string.IsNullOrEmpty(Input) && int.TryParse(Input, out int result);
+        }
+
+        private void Restart(object obj)
+        {
+            InitGame();
+        }
+
+
+        private void SuccessfullGuess()
+        {
+            Message = $"You guessed correct with {game.NumberOfGuesses} tries";
+            GameOn = false;
+        }
+
+        private void InitGame()
+        {
+            game = new GuessingGame();
+            Input = "";
+            GameOn = true;
+            Message = "Guess a number between 1 and 100";
+
+        }
 
         protected void SetProperty(string value, ref string property, string name)
         {
---
+++ GuessTheNumber.VM/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GuessTheNumber.VM
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _excute;
+        private Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _excute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+                return true;
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _excute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
